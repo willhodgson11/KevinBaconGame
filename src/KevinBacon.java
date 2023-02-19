0fab36b0fa226bc9509729d982a4af47943a43ae
willhodgson <willhodgson11@gmail.com>
2023-02-19 16:11:56 -0500

    start KB

--- src/KevinBacon.java
+++ src/KevinBacon.java
@@ -1,12 +1,23 @@
 import java.io.*;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class KevinBacon {
-    private AdjacencyMapGraph<String, String> graph;
+    private AdjacencyMapGraph<String, Set<String>> graph;
 
-    public AdjacencyMapGraph<String, String> getGraph(String actorsFile, String moviesFile, String movieActorFile) throws IOException {
+    public KevinBacon(String actorsFile, String moviesFile, String movieActorFile) throws IOException {
+        GraphBuilder graphy = new GraphBuilder(actorsFile, moviesFile, movieActorFile);
+        this.graph = graphy.buildGraph();
+        welcome();
+    }
 
-        return graph;
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        KevinBacon test0 = new KevinBacon("actorsTest.txt", "moviesTest.txt", "movie-actorsTest.txt");
+
     }
 }
